fe683830db4001925c9237e229c09db9081a5dc7
xaviour-sketch <xaviourmuthee24@gmail.com>
2026-06-10 08:27:39 +0300

    new commit

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,35 @@
 #!/usr/bin/env python3
 
 class Book:
-    pass
+    """A class to represent a book."""
     
+    def __init__(self, title, page_count):
+        """
+        Initialize a Book instance.
         
+        Args:
+            title (str): The title of the book
+            page_count (int): The number of pages in the book
+        """
+        self.title = title
+        self.page_count = page_count
+    
+    @property
+    def page_count(self):
+        """Get the page count of the book."""
+        return self._page_count
+    
+    @page_count.setter
+    def page_count(self, value):
+        """
+        Set the page count of the book.
+        Validates that the value is an integer.
+        """
+        if isinstance(value, int):
+            self._page_count = value
+        else:
+            print("page_count must be an integer")
+    
+    def turn_page(self):
+        """Print a message when turning a page."""
+        print("Flipping the page...wow, you read fast!")
--- lib/coffee.py
+++ lib/coffee.py
@@ -1,4 +1,37 @@
 #!/usr/bin/env python3
 
 class Coffee:
-    pass
+    """A class to represent a coffee product."""
+    
+    def __init__(self, size, price):
+        """
+        Initialize a Coffee instance.
+        
+        Args:
+            size (str): The size of the coffee (Small, Medium, or Large)
+            price (float): The price of the coffee
+        """
+        self.size = size
+        self.price = price
+    
+    @property
+    def size(self):
+        """Get the size of the coffee."""
+        return self._size
+    
+    @size.setter
+    def size(self, value):
+        """
+        Set the size of the coffee.
+        Validates that the size is Small, Medium, or Large.
+        """
+        valid_sizes = ["Small", "Medium", "Large"]
+        if value in valid_sizes:
+            self._size = value
+        else:
+            print("size must be Small, Medium, or Large")
+    
+    def tip(self):
+        """Add a tip to the coffee and print a message."""
+        print("This coffee is great, here\u2019s a tip!")
+        self.price += 1
